913c71f34a5ad6356831a78e5c258d118d6b3b5f
Manjari Prithani <manjariprithani1408@gmail.com>
2022-05-08 18:10:58 +0530

    Bootstrap added

---
+++ src/Container/HomePage.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import "./HomePage.scss";
+
+export default function HomePage(props) {
+
+    return (
+        <div className='homePage'>
+            <div class="column">
+                <div class="column-title">TODO</div>
+                <div class="card">
+                    <div class="card-title">Card 1</div>
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
